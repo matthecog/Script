4eb4363723e004b3acb668551e614189b940d623
Matheus Guimarães <107039944+matthecog@users.noreply.github.com>
2024-11-28 16:02:37 -0300

    Update Apagar arquivos SEM confirmação.ps1

--- Apagar arquivos por extenção e data/Apagar arquivos SEM confirmação.ps1
+++ Apagar arquivos por extenção e data/Apagar arquivos SEM confirmação.ps1
@@ -1,14 +1,14 @@
-﻿# Lista de diretórios
-$directories = @(
-# Separar diretórios por vírgola
-# Adicione mais diretórios conforme necessário
+#Lista de diretórios
+    $directories = @(
+    # Separar diretórios por vírgola
+    # Adicione mais diretórios conforme necessário
     "C:\Teste\"
-)
-# Extensão do arquivo e data limite
-$fileExtension = ".log"
-# Data tem duas opções, a segunda está comentada.
-$dateLimit = Get-Date "09/01/2024"
-#date_limit = Get-Date).AddDays(-7)
+    )
+#Extensão do arquivo e data limite
+    $fileExtension = ".log"
+    # Data tem duas opções, a segunda está comentada.
+    $dateLimit = (Get-Date).AddDays(-7)
+   #$dateLimit = Get-Date "10/15/2024"
 
 foreach ($directory in $directories) {
     Get-ChildItem -Path $directory -Recurse -Filter "*$fileExtension" | Where-Object {
