bdd19d55add07b8e04fbd942544139f511dd12a4
Matheus Guimarães <107039944+matthecog@users.noreply.github.com>
2024-11-28 14:28:07 -0300

    Update apagar arquivos COM confirmação 1 à 1.ps1

--- Apagar arquivos por extenção e data/Apagar arquivos COM confirmação 1 à 1.ps1
+++ Apagar arquivos por extenção e data/Apagar arquivos COM confirmação 1 à 1.ps1
@@ -1,12 +1,14 @@
 # Lista de diretórios
 $directories = @(
-    "C:\Teste\"
-    # Adicione mais diretórios conforme necessário
+#Separar diretórios por vírgola
+# Adicione mais diretórios conforme necessário
+    "C:\Teste\", "D:\Teste\","E:\Teste\"
 )
-
 # Extensão do arquivo e data limite
 $fileExtension = ".log"
+#Data tem duas opções, a segunda está comentada.
 $dateLimit = Get-Date "09/01/2024"
+#date_limit = Get-Date).AddDays(-7)
 
 foreach ($directory in $directories) {
     Get-ChildItem -Path $directory -Recurse -Filter "*$fileExtension" | Where-Object {
